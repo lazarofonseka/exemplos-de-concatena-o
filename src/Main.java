9a2944e5e2c31da0bb931f1d71ca78bb5c90a028
Lazaro Fonseca <lazaroffsilva87@gmail.com>
2024-09-22 01:08:43 -0300

    exemplos concatenation
    Signed-off-by: Lazaro Fonseca <lazaroffsilva87@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        String concatenacao = "?";
+        String concatenacao;
         concatenacao = 1+1+1+"1";
         System.out.println(concatenacao);
         concatenacao = 1+"1"+1+1;
